b64506608df393c3c223834647c976dc9593f8d1
Alan Engel <ttcrossroads09@gmail.com>
2020-12-04 12:43:01 +0900

    move ref script to js file

--- ShowReferences.js
+++ ShowReferences.js
@@ -1,54 +1,12 @@
-function ShowReference(ref)
+function ShowReference(refcite)
 {
-modal = document.getElementById("project-references");
-var z, elmnt, label;
-if(modal)
-{
-if(modal.innerHTML.length == 0)
-{
-LoadReferences(ref);
-return true;
-}
-z = modal.getElementsByTagName("div");
-for(i = 0; i < z.length; i++)
-{
-elmnt = z[i];
-label = elmnt.getAttribute("id");
-if(label )
-{
-if( label == ref)
-{
-    var x = elmnt.getElementsByClassName("close");
-    if(x.length == 0)
-    {
-        elmnt.insertAdjacentHTML("afterbegin","<span class='close'>&times;</span>")
-        elmnt.insertAdjacentHTML("beforeend","<p><a href='RefList.html' target='_blank'>Show full project reference list.</p>")
-    }
-span = document.getElementsByClassName("close")[0];
-span.onclick = function() {
-    modal.style.display = "none";
-}
-elmnt.setAttribute("class", "modal-content");
-modal.style.display = "block";
-return true;
-}
-}
-}
-}
-
-return false;
-}
-// Get the modal
-var modal = document.getElementById("project-references");
-
-function LoadReferences(ref) {
-var z, i, elmnt, file, xhttp;
+  var z, i, elmnt, file, xhttp;
 /* Loop through a collection of all HTML elements: */
 z = document.getElementsByClassName("reference-content")
 for (i = 0; i < z.length; i++) {
 elmnt = z[i];
 /*search for elements with a certain atrribute:*/
-file = elmnt.getAttribute("w3-include-html");
+file = elmnt.getAttribute("w3-include-xml");
 if (file) {
 /* Make an HTTP request using the attribute value as the file name: */
 xhttp = new XMLHttpRequest();
@@ -56,12 +14,9 @@ xhttp.onreadystatechange = function() {
 if (this.readyState == 4) {
 if (this.status == 200) 
 {
-LoadReferenceDocument(this,ref);
+  getResult(this.responseXML, refcite);
 }
 if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-/* Remove the attribute, and call this function once more: */
-elmnt.removeAttribute("w3-include-html");
-LoadReferences();
 }
 }
 xhttp.open("GET", file, true);
@@ -72,8 +27,47 @@ return;
 }
 }
 
-function LoadReferenceDocument(xhttp, ref) {
-document.getElementsByClassName("reference-content")[0].innerHTML =
-xhttp.responseText;
-if(ref != null){ShowReference(ref);}
+function getResult(xml,refcite)
+{
+  var divwrapper = "<div id='reference' class='modal-content'><span class='close'>&times;</span>";
+  var txt = "";
+  var path = "/references/reference[@id='" + refcite + "']";
+//xml.path  = "/references/reference[@id='" + refcite + "']";
+  if(xml.evaluate)
+  {
+    var nodes = xml.evaluate(path,xml,null,XPathResult.ANY_TYPE, null);
+    var result = nodes.iterateNext();
+        while (result) {
+            txt += result.innerHTML;
+            result = nodes.iterateNext();
+        } 
+  } else if (window.ActiveXObject || xhttp.responseType == "msxml-document") {
+        xml.setProperty("SelectionLanguage", "XPath");
+        nodes = xml.selectNodes(path);
+        for (i = 0; i < nodes.length; i++) {
+            txt += nodes[i].nodeValue;
+        }
+    }
+
+    if(txt.length > 0)
+    {
+    divwrapper += txt + "</div>";
+
+    document.getElementById("project-references").innerHTML = divwrapper;  
+    span = document.getElementsByClassName("close")[0];
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    modal.style.display="block"
+  }
+}
+
+// Get the modal
+var modal = document.getElementById("project-references");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
