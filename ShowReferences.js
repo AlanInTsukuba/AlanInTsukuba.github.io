e5b3b37473b76f83ee61f3b4ab4a01d5df3217d8
Alan Engel <ttcrossroads09@gmail.com>
2020-12-04 14:03:00 +0900

    move modal event assign

--- ShowReferences.js
+++ ShowReferences.js
@@ -64,13 +64,13 @@ function getResult(xml,refcite)
         modal.style.display = "none";
     }
     // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+
     modal.style.display="block"
   }
 }
 
-
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
